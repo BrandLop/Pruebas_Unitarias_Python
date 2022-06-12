8e0dc5ffebd79464a58abbaa7da81b796dcade98
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-06-11 22:53:03 -0500

    Adding docstring to function

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # Docstring
 
 def palindrome(sentence: str) -> bool:
+    """Check if a sentence is a palindrome.
 
+    Args:
+        sentence (str): The sentence to check.
+
+    Returns:
+        bool: True if the sentence is a palindrome, False otherwise.
+    """
     sentence = sentence.replace(" ", "").lower()
     return sentence == sentence[::-1]
