23ff97adcdaf7fef21f54501f37859dd3bd13946
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-06-12 21:30:01 -0500

    Testing another alternative to assert

--- Unittest/main.py
+++ Unittest/main.py
@@ -2,7 +2,10 @@
 
 if __name__ == '__main__':
     try:
-        assert 10 == 9, '10 != 9'
+        # assert 10 == 9, '10 != 9'
+        # This is the same as previous line
+        if not 10 == 9:
+            raise AssertionError('10 != 9')
         print('>>> The program is still running')
     except AssertionError as e:
         print(e)
