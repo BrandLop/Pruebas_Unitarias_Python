cb0c2a14f39f4410baae298a09d54d126cb4a9f7
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-06-12 21:40:16 -0500

    Adding assert to the function

--- Unittest/AssertP2.py
+++ Unittest/AssertP2.py
@@ -1,5 +1,5 @@
 def sum_numbers(n1: int, n2: int) -> int:
-    """Sum two numbers
+    """Sum two numbers positives
 
     Args:
         n1 (int): Number 1
@@ -8,8 +8,9 @@ def sum_numbers(n1: int, n2: int) -> int:
     Returns:
         int: Sum of n1 and n2
     """
+    assert n1 > 0 and n2 > 0, "Both numbers must be positive"
     return n1 + n2
 
 if __name__ == '__main__':
-    res = sum_numbers(10, 20)
+    res = sum_numbers(-10, 20)
     print(res)
