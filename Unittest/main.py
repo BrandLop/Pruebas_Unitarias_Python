af1e3c3c05ba477d77561bf2f37b077e1ca4d2a6
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-06-12 21:26:37 -0500

    Adding try except

--- Unittest/main.py
+++ Unittest/main.py
@@ -1,5 +1,8 @@
 # assert
 
 if __name__ == '__main__':
-    assert 10 == 9, '10 != 9'
-    print('>>> The program is still running')
+    try:
+        assert 10 == 9, '10 != 9'
+        print('>>> The program is still running')
+    except AssertionError as e:
+        print(e)
